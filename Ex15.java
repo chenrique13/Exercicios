c2cf24ed847a9f64de8441560f2fbd6e8dd38611
Carlos Pereira <56885446+chenrique13@users.noreply.github.com>
2019-11-14 17:28:15 -0300

    Create Ex15.java

---
+++ Ex15.java
@@ -0,0 +1,31 @@
+package ex1;
+
+import java.util.Scanner;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+	
+		Scanner scanner = new Scanner(System.in);
+		
+		int a;
+		int b;
+		int c;
+		int d;
+		
+		System.out.println("Digite o primeiro numero: ");
+		a = scanner.nextInt();
+		
+		System.out.println("Digite o segundo numero: ");
+		b = scanner.nextInt();
+		
+		c = a%b;
+		d = a/b;
+		
+		System.out.println(a + " / " + b 
+				+ " o resultado da divisao � " + d
+				+ " e o resto da divisao � " + c);
+		
+	}
+
+}
